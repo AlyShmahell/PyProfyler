36409d856a80637ec7170574ed358efd23bf910b
AlyShmahell <aly.shmahell@gmail.com>
2019-03-31 06:54:04 +0200

    moved PyProfyler to its own repo

---
+++ pyprofyler/__init__.py
@@ -0,0 +1,1 @@
+from pyprofyler import PyProfyler
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+	name='pyprofyler',
+	version='0.1.0',
+	author='Aly Shmahell',
+	author_email='aly.shmahell@gmail.com',
+	description='a simple memory profiler for python programs.',
+	license="Copyrights © 2019 Aly shmahell.",
+	packages=['pyprofyler'],
+	install_requires=[
+			  "psutil==5.6.1",
+			  "cython==0.29.6",
+			  "numpy==1.16.2"
+			]
+)
