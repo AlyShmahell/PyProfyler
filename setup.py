6f263fbfc3bab44a543a0d4e73fbab3f239fa93b
AlyShmahell <aly.shmahell@gmail.com>
2019-04-03 08:09:15 +0200

    finalized pypi upload

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from os.path import exists
 from setuptools import setup
 setup(
 	name='pyprofyler',
-	version='1.0.3',
+	version='1.0.4',
 	author='Aly Shmahell',
 	author_email='aly.shmahell@gmail.com',
 	url='https://github.com/AlyShmahell/PyProfyler',
 	description='a simple memory profiler for python programs.',
-	long_description=(open('README.rst').read() if exists('README.rst')
+	long_description=(open('README.md', encoding='utf-8').read() if exists('README.md')
                         else ''),
+    long_description_content_type='text/markdown',
 	packages=['pyprofyler'],
 	install_requires=[
 			  "psutil==5.6.1",
