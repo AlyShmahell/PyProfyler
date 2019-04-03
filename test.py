4f41203f0e0623cb3f4048acb706a20f25213266
AlyShmahell <aly.shmahell@gmail.com>
2019-04-03 06:42:11 +0200

    finalized PyProfyler

--- test.py
+++ test.py
@@ -1,26 +1,41 @@
-from profile import Profiler
+from pyprofyler import PyProfyler
 
-def delayed_function():
+def wrapped_function():
     array = []
-    for i in range(1,100000):
+    for i in range(1,1000000):
         array.append(i)
     return array
 
-@Profiler
+@PyProfyler
 def decorated_function():
     a_list = []
-    for i in range(1,100000):
+    for i in range(1,1000000):
         a_list.append(i)
     return a_list
 
 if __name__ == '__main__':
-    delayed_profiler = Profiler(delayed_function)
-    print(delayed_profiler)
-    result = delayed_profiler()
-    print(delayed_profiler)
+    ##############################################
+    #    Profiling a function by wrapping it     #
+    ##############################################
+    wrapped_profile = PyProfyler(wrapped_function)
+    # "Not Profiled Yet" Message
+    print(wrapped_profile)
+    result = wrapped_profile()
+    # Profile Message Appears
+    print(wrapped_profile)
+    # Function Execution Result
     print(f"execution result: {result[10]}")
+    # Profile Message, through __getitem__
+    print(wrapped_profile['profile'])
+    ##############################################
+    #      Profiling a decorated function        #
+    ##############################################
+    # "Not Profiled Yet" Message
     print(decorated_function)
     result = decorated_function()
+    # Profile Message, through __str__
     print(decorated_function)
+    # Function Execution Result
     print(f"execution result: {result[10]}")
-    print(decorated_function['profsile'])
+    # Profile Message, through __getitem__
+    print(decorated_function['profile'])
